9930e999d3fd3a44c13fa37a92ec07d0515fb264
Andrea Ulicna <andrea@ulicna.com>
2024-02-22 18:33:59 +0100

    Fixed lone rays getting casted 180 degrees off and correct detection of horizontal vs vertical lines by adding almost functions to all comparisons. 3D walls still aren't always the correct height and color.

--- my_own_raycaster.c
+++ my_own_raycaster.c
@@ -6,22 +6,35 @@
 /*   By: aulicna <aulicna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:48:33 by aulicna           #+#    #+#             */
-/*   Updated: 2024/02/22 17:28:44 by aulicna          ###   ########.fr       */
+/*   Updated: 2024/02/22 18:26:58 by aulicna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./inc/cub3d.h"
+
+bool almost_equal(double a, double b, double epsilon)
+{
+	return (fabs(a - b) < epsilon);
+}
+
+bool almost_less_than(double a, double b, double epsilon) {
+	return (b - a > epsilon);
+}
+
+bool almost_greater_than(double a, double b, double epsilon) {
+	return (a - b > epsilon);
+}
 
 int	init_game_struct(t_game *game)
 {
 	game->map = calloc(1, sizeof(t_map));
 	game->map->grid = calloc(9, sizeof(char *)); // init the map
 	game->map->grid[0] = ft_strdup("11111111"); //fill the map
-	game->map->grid[1] = ft_strdup("10000001");
-	game->map->grid[2] = ft_strdup("10101111");
-	game->map->grid[3] = ft_strdup("11001001");
+	game->map->grid[1] = ft_strdup("10100001");
+	game->map->grid[2] = ft_strdup("10100001");
+	game->map->grid[3] = ft_strdup("10100001");
 	game->map->grid[4] = ft_strdup("10000001");
-	game->map->grid[5] = ft_strdup("10000001");
+	game->map->grid[5] = ft_strdup("10000101");
 	game->map->grid[6] = ft_strdup("10000001");
 	game->map->grid[7] = ft_strdup("11111111");
 	game->map->grid[8] = NULL;
@@ -114,7 +127,7 @@ void	handle_key_actions(void *param)
     if (mlx_is_key_down(game->mlx, MLX_KEY_LEFT))
     {
         player->p_a -= ROTATION_SPEED;
-        if (player->p_a < 0)
+        if (almost_less_than(player->p_a, 0, PRECISION))
             player->p_a += 2 * M_PI;
         player->p_dx = cos(player->p_a) * 5;
         player->p_dy = sin(player->p_a) * 5;
@@ -122,7 +135,7 @@ void	handle_key_actions(void *param)
     if (mlx_is_key_down(game->mlx, MLX_KEY_RIGHT))
     {
         player->p_a += ROTATION_SPEED;
-        if (player->p_a > 2 * M_PI)
+        if (almost_greater_than(player->p_a, (2 * M_PI), PRECISION))
             player->p_a -= 2 * M_PI;
         player->p_dx = cos(player->p_a) * 5;
         player->p_dy = sin(player->p_a) * 5;
@@ -282,11 +295,6 @@ double	distance_player_ray_end(int p_x, int p_y, int r_x, int r_y)
 	return (sqrt((r_x - p_x) * (r_x - p_x) + (r_y - p_y) * (r_y - p_y)));
 }
 
-int almost_equal(double a, double b, double epsilon)
-{
-	return (fabs(a - b) < epsilon);
-}
-
 void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 {
 	int	r, dof, mx, my, mp;
@@ -307,12 +315,12 @@ void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 	// initialize ray angle DEGREE degrees back from the player angle
 	ray->r_a = player->p_a - DEGREE * 30;
 	// then we need to add the limits to the next angle bcs unit circle
-	if (ray->r_a < 0)
+	if (almost_less_than(ray->r_a, 0, PRECISION))
 		ray->r_a += 2 * M_PI;
-	if (ray->r_a >= 2 * M_PI)
+	if (almost_greater_than(ray->r_a, 2 * M_PI, PRECISION))
 		ray->r_a -= 2 * M_PI;
 	// how many rays will be drawn
-	for (r = 0; r < 60; r++)
+	for (r = 0; r < FOV; r++)
 	{
 		// HORIZONTAL
 		// check horizontal lines - where the ray will first hit the closest horizontal line
@@ -321,7 +329,7 @@ void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 		int	hx = player->p_x;
 		int	hy = player->p_y;
 		double aTan = -1 / tan(ray->r_a);
-		if (ray->r_a > M_PI) // ray looking up - determined from the ray angle
+		if (almost_greater_than(ray->r_a, M_PI, PRECISION)) // ray looking up - determined from the ray angle
 		{
 			ray->r_y = (((int)player->p_y >> 6) << 6) - 0.0001;
 			ray->r_x = (player->p_y - ray->r_y) * aTan + player->p_x;
@@ -329,7 +337,7 @@ void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 			ray->y_offset = (-TILE_SIZE);
 			ray->x_offset = (-ray->y_offset) * aTan;
 		}
-		if (ray->r_a < M_PI) // ray looking down - determined from the ray angle
+		if (almost_less_than(ray->r_a, M_PI, PRECISION)) // ray looking down - determined from the ray angle
 		{
 			ray->r_y = (((int)player->p_y >> 6) << 6) + TILE_SIZE;
 			ray->r_x = (player->p_y - ray->r_y) * aTan + player->p_x;
@@ -346,7 +354,7 @@ void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 	//	if (ray->r_a == 0 || ray->r_a == M_PI) // if the ray is looking straight left or right, it will never hit a horizontal line
 	//	{
 	//		ray->r_x = player->p_x;
-	//		ray->r_y = player->p_y;
+	//		ray->r_y = ppixelslayer->p_y;
 	//		dof = 8;
 	//	}
 		while (dof < 8)
@@ -382,7 +390,7 @@ void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 		int	vx = player->p_x;
 		int	vy = player->p_y;
 		double nTan = (-tan(ray->r_a));
-		if (ray->r_a > (M_PI / 2) && ray->r_a < (3 * M_PI / 2)) // ray looking left - determined from the ray angle
+		if (almost_greater_than(ray->r_a, (M_PI / 2), PRECISION) && almost_less_than(ray->r_a, (3 * M_PI / 2), PRECISION)) // ray looking left - determined from the ray angle
 		{
 			ray->r_x = (((int)player->p_x >> 6) << 6) - 0.0001;
 			ray->r_y = (player->p_x - ray->r_x) * nTan + player->p_y;
@@ -390,7 +398,7 @@ void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 			ray->x_offset = (-TILE_SIZE);
 			ray->y_offset = (-ray->x_offset) * nTan;
 		}
-		if (ray->r_a < (M_PI / 2) || ray->r_a > (3 * M_PI / 2)) // ray looking right - determined from the ray angle
+		if (almost_less_than(ray->r_a, (M_PI / 2), PRECISION) || almost_greater_than(ray->r_a, (3 * M_PI / 2), PRECISION)) // ray looking right - determined from the ray angle
 		{
 			ray->r_x = (((int)player->p_x >> 6) << 6) + TILE_SIZE;
 			ray->r_y = (player->p_x - ray->r_x) * nTan + player->p_y;
@@ -429,14 +437,14 @@ void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 	//	printf("r_y: %d %d\n", ray->r_y, ray->r_y >> 6);
 	//	printf("r_x: %d %d\n", ray->r_x, ray->r_x >> 6);
 	// pick the shortest distance
-		if (distV < distH)
+		if (almost_less_than(distV, distH, PRECISION))
 		{
 			ray->r_x = vx;
 			ray->r_y = vy;
 			distT = distV;
 			color = 0xFF0000FF;
 		}
-		if (distH < distV)
+		if (almost_less_than(distH, distV, PRECISION))
 		{
 			ray->r_x = hx;
 			ray->r_y = hy;
@@ -447,14 +455,14 @@ void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 		// 3D WALLS
 		// distance between the player angle and the ray angle
 		double ca = player->p_a - ray->r_a;
-		if (ca < 0)
+		if (almost_less_than(ca, 0, PRECISION))
 			ca += 2 * M_PI;
-		if (ca >= 2 * M_PI)
+		if (almost_greater_than(ca, (2 * M_PI), PRECISION))
 			ca -= 2 * M_PI;
 		// multiply the ray distance by the cosine of that ray angle
 		distT = distT * cos(ca); // fix fish eye
 		double lineH = (TILE_SIZE * 320) / distT; // line height
-		if (lineH > 320)
+		if (almost_greater_than(lineH, 320, PRECISION))
 			lineH = 320;
 		double lineO = 160 - lineH / 2; // line offset
 		draw_line_thickness(r * map->map_width + 530, lineO, r * map->map_height + 530, lineH + lineO, color, 5, game->image);
@@ -462,9 +470,9 @@ void	draw_rays(t_game *game, t_map *map, t_player *player, t_ray *ray)
 		// if drawing more lines, the angle need to more after each line
 		ray->r_a += DEGREE;
 		// and the limits need to be set again bcs unit circle
-		if (ray->r_a < 0)
+		if (almost_less_than(ray->r_a, 0, PRECISION))
 			ray->r_a += 2 * M_PI;
-		if (ray->r_a >= 2 * M_PI)
+		if (almost_greater_than(ray->r_a, 2 * M_PI, PRECISION))
 			ray->r_a -= 2 * M_PI;
 	}
 }
